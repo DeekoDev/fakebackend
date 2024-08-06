9b455a19dc55b14f481ed1e2d69a437beb23ceed
DiegoCadavid <dcmgames50@gmail.com>
2024-08-06 04:03:40 -0500

    feat: community page responsive

--- src/app/(app)/community/page.tsx
+++ src/app/(app)/community/page.tsx
@@ -13,7 +13,7 @@ const CommunityPage = async ({}: Props) => {
         Explore the projects created by the community.
       </p>
 
-      <div className="mt-8 grid grid-cols-3">
+      <div className="mt-8 grid  grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {data.map((project) => {
           return <ProjectCard key={project.id} {...project} />;
         })}
